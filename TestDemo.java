968ffa5cbd2f26d282177dfe7424c89877a965da
Jinke Wang <OK580@users.noreply.github.com>
2019-03-11 11:15:31 +0800

    Create TestDemo.java

---
+++ TestDemo.java
@@ -0,0 +1,13 @@
+package com.zte.soc.securityperformance.model.bugmanage;
+
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
